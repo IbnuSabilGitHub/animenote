66f433f481d000d978dee4ccca9d054ff1105ea5
Ibnu Shabill Al Zahari <2411102441264@umkt.ac.id>
2025-02-20 20:12:14 +0800

    setup serverless function

---
+++ api/hello.ts
@@ -0,0 +1,5 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  return res.json({ message: "Hello from Serverless!" });
+}

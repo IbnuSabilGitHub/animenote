e792fc52778c8286639249f8e4c1979a6dcc01b9
IbnuSabilGitHub <ibnusabil2301@gmail.com>
2025-02-22 23:38:14 +0800

    add: component DecryptedText

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import reactLogo from "../../assets/svg/react.svg";
 import viteLogo from "../../assets/svg/vite.svg";
-import vercelLogo from "../../assets/svg/vercel.svg"
+import vercelLogo from "../../assets/svg/vercel.svg";
 import tailwindLogo from "../../assets/svg/tailwind.svg";
 import muiLogo from "../../assets/svg/mui.svg";
 import "../../styles/home.css";
 import "../../components/ShinyText";
 import ShinyText from "../../components/ShinyText";
-
+import DecryptedText from "../../components/DecryptedText";
 
 function Home() {
   const [count, setCount] = useState(0);
@@ -15,23 +15,55 @@ function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen text-white">
       <div className="flex gap-4">
-      <a href="https://vercel.com/docs" target="_blank">
-          <img src={vercelLogo} className="w-20 h-20 hover:scale-110 transition-transform" alt="Vite logo" />
+        <a href="https://vercel.com/docs" target="_blank">
+          <img
+            src={vercelLogo}
+            className="w-20 h-20 hover:scale-110 transition-transform"
+            alt="Vite logo"
+          />
         </a>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="w-20 h-20 hover:scale-110 transition-transform" alt="Vite logo" />
+          <img
+            src={viteLogo}
+            className="w-20 h-20 hover:scale-110 transition-transform"
+            alt="Vite logo"
+          />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="w-20 h-20 hover:scale-110 transition-transform logo" alt="React logo" />
+          <img
+            src={reactLogo}
+            className="w-20 h-20 hover:scale-110 transition-transform logo"
+            alt="React logo"
+          />
         </a>
         <a href="https://mui.com/material-ui/getting-started/" target="_blank">
-          <img src={muiLogo} className="w-20 h-20 hover:scale-110 transition-transform" alt="Vite logo" />
+          <img
+            src={muiLogo}
+            className="w-20 h-20 hover:scale-110 transition-transform"
+            alt="Vite logo"
+          />
         </a>
-        <a href="https://tailwindcss.com/docs/installation/using-vite" target="_blank">
-          <img src={tailwindLogo} className="w-20 h-20 hover:scale-110 transition-transform" alt="Vite logo" />
+        <a
+          href="https://tailwindcss.com/docs/installation/using-vite"
+          target="_blank"
+        >
+          <img
+            src={tailwindLogo}
+            className="w-20 h-20 hover:scale-110 transition-transform"
+            alt="Vite logo"
+          />
         </a>
       </div>
-      <h1 className="text-3xl font-bold mt-6">ANIME NOTE</h1>
+      {/* <h1 className="text-3xl font-bold mt-6">ANIME NOTE</h1> */}
+      <DecryptedText
+        text="Anime Note"
+        animateOn="view"
+        revealDirection="center"
+        speed={200}
+        parentClassName="mt-6"
+        className="text-3xl font-bold"
+        encryptedClassName="text-3xl font-bold"
+      />
       <div className="mt-4 p-6 bg-zinc-800 rounded-lg shadow-lg text-center">
         <button
           onClick={() => setCount((count) => count + 1)}
@@ -40,10 +72,16 @@ function Home() {
           count is {count}
         </button>
         <p className="mt-3 text-gray-300">
-          Hello, <code className="text-pink-400">user</code> welcome to Anime Note
+          Hello, <code className="text-pink-400">user</code> welcome to Anime
+          Note
         </p>
       </div>
-      <ShinyText text="Click on logos to learn more" disabled={false} speed={3} className='custom-class'/>
+      <ShinyText
+        text="Click on logos to learn more"
+        disabled={false}
+        speed={3}
+        className="custom-class"
+      />
     </div>
   );
 }

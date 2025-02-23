b087e9db51652b2b70a12861fa492aa473c04d10
IbnuSabilGitHub <ibnusabil2301@gmail.com>
2025-02-23 19:43:09 +0800

    create: create a variant Carousel

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,19 +1,20 @@
 import "../../styles/home.css";
 import "../../components/ShinyText";
-import Carousel from "../../components/Carousel";
 import ShinyText from "../../components/ShinyText";
+import SpotlightCarousel from "../../components/SoptLightCarousel";
 
 function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen text-white">
       <div className="flex flex-col items-center justify-center space-y-8">
-        <Carousel
+        <SpotlightCarousel
           baseWidth={300}
           autoplay={true}
           autoplayDelay={3000}
           pauseOnHover={true}
           loop={true}
           round={false}
+          spotlightColor="rgba(0, 132, 209, 0.25)"
         />
       <ShinyText text="Technology" className="text-3xl font-bold mb-4" />
       </div>

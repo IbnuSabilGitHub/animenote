72e742d9eab84b83e47c95d5ad664e599b899ca2
IbnuSabilGitHub <ibnusabil2301@gmail.com>
2025-02-24 20:35:24 +0800

    change: change ui navbar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,56 +1,65 @@
-import { Outlet, Link } from "react-router-dom";
-import { useState } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+// import { useState} from "react";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import Box from "@mui/material/Box";
-import BottomNavigation from "@mui/material/BottomNavigation";
-import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import drakTheme from "../theme";
+import Dock from "./Dock";
+
+interface DockItem {
+  icon: JSX.Element;
+  label: string;
+  onClick: () => void;
+  className: string;
+}
+
+const items = (navigate: (path: string) => void): DockItem[] => [
+  {
+    icon: <i className="fa-solid fa-house fa-xs"></i>,
+    label: "Home",
+    onClick: () => navigate("/"),
+    className: "rounded-lg",
+  },
+  {
+    icon: <i className="fa-solid fa-person fa-xs"></i>,
+    label: "About",
+    onClick: () => navigate("/About"),
+    className: "rounded-lg",
+  },
+  {
+    icon: <i className="fa-solid fa-circle-user fa-xs"></i>,
+    label: "Profile",
+    onClick: () => navigate("/user/ADMIN"),
+    className: "rounded-lg",
+  },
+  {
+    icon: <i className="fa-solid fa-gear fa-xs"></i>,
+    label: "Settings",
+    onClick: () => navigate("/Settings"),
+    className: "rounded-lg",
+  },
+];
 
 export default function Layout() {
-  const [value, setValue] = useState(0);
+  // const [value, setValue] = useState(0);
+  const navigate = useNavigate();
+
   return (
     <ThemeProvider theme={drakTheme}>
       <CssBaseline />
-      <Box
-        sx={{
-          // display: "flex",
-          // flexDirection: "column-reverse",
-          height: "100vh",
-        }}
-      >
-        {/* Sidebar */}
-        <Box
-          sx={{
-            width: 300,
-            mb: 2,
-            position: "fixed",
-            bottom: 0,
-            left: "50%",
-            transform: "translateX(-50%)",
-          }}
-        >
-          <BottomNavigation
-            showLabels
-            value={value}
-            onChange={(_, newValue) => {
-              setValue(newValue);
-            }}
-            sx={{
-              borderRadius:2 ,
-              fontWeight: "bold",
-              bgcolor: "rgba(255, 255, 255, 0.12)",
-            }}
-          >
-            <BottomNavigationAction component={Link} to="/" label="Home" />
-            <BottomNavigationAction component={Link} to="/about" label="About" />
-            <BottomNavigationAction component={Link} to="/user/ADMIN" label="User" />
-          </BottomNavigation>
-        </Box>
+      <div>
+        <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 mb-1">
+        <Dock
+          items={items(navigate)}
+          panelHeight={68}
+          baseItemSize={50}
+          magnification={70}
+        />
+        </div>
+
         {/* Konten Utama */}
-        <Box sx={{ flex: 1, p: 1}}>
+        <div>
           <Outlet /> {/* Ini tempat konten halaman akan ditampilkan */}
-        </Box>
-      </Box>
+        </div>
+      </div>
     </ThemeProvider>
   );
 }

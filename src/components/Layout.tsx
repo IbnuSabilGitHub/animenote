22b830e47e2ebb1abe5ad1cf0c17228731a6e85a
IbnuSabilGitHub <ibnusabil2301@gmail.com>
2025-02-25 20:27:57 +0800

    fix: fixed a conflict due to removing a dependency

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,5 @@
 import { Outlet, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import drakTheme from "../theme";
 import Dock from "./Dock";
 
 interface DockItem {
@@ -13,25 +11,25 @@ interface DockItem {
 
 const items = (navigate: (path: string) => void): DockItem[] => [
   {
-    icon: <i className="fa-solid fa-house text-xs md:text-sm"></i>,
+    icon: <i className="fa-solid fa-house text-xs md:text-sm text-amber-50"></i>,
     label: "Home",
     onClick: () => navigate("/"),
     className: "rounded-lg active:bg-neutral-900 transition-colors",
   },
   {
-    icon: <i className="fa-solid fa-person text-xs md:text-sm"></i>,
+    icon: <i className="fa-solid fa-person text-xs md:text-sm text-amber-50"></i>,
     label: "About",
     onClick: () => navigate("/About"),
     className: "rounded-lg active:bg-neutral-900 transition-colors",
   },
   {
-    icon: <i className="fa-solid fa-circle-user text-xs md:text-sm"></i>,
+    icon: <i className="fa-solid fa-circle-user text-xs md:text-sm text-amber-50"></i>,
     label: "Profile",
     onClick: () => navigate("/user/ADMIN"),
     className: "rounded-lg active:bg-neutral-900 transition-colors",
   },
   {
-    icon: <i className="fa-solid fa-gear text-xs md:text-sm"></i>,
+    icon: <i className="fa-solid fa-gear text-xs md:text-sm text-amber-50"></i>,
     label: "Settings",
     onClick: () => navigate("/Settings"),
     className: "rounded-lg active:bg-neutral-900 transition-colors",
@@ -55,32 +53,29 @@ export default function Layout() {
   }, []);
 
   return (
-    <ThemeProvider theme={drakTheme}>
-      <CssBaseline />
-      <div>
-        <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 mb-1">
-          {width > 768 ? (
-            <Dock
-              items={items(navigate)}
-              panelHeight={70}
-              baseItemSize={50}
-              magnification={70}
-            />
-          ) : (
-            <Dock
-              items={items(navigate)}
-              panelHeight={60}
-              baseItemSize={40}
-              magnification={60}
-            />
-          )}
-        </div>
-
-        {/* Konten Utama */}
-        <div>
-          <Outlet /> {/* Ini tempat konten halaman akan ditampilkan */}
-        </div>
+    <div>
+      <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 mb-1">
+        {width > 768 ? (
+          <Dock
+            items={items(navigate)}
+            panelHeight={70}
+            baseItemSize={50}
+            magnification={70}
+          />
+        ) : (
+          <Dock
+            items={items(navigate)}
+            panelHeight={60}
+            baseItemSize={40}
+            magnification={60}
+          />
+        )}
       </div>
-    </ThemeProvider>
+
+      {/* Konten Utama */}
+      <div>
+        <Outlet /> {/* Ini tempat konten halaman akan ditampilkan */}
+      </div>
+    </div>
   );
 }

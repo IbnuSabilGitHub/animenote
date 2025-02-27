cedb517a64976d4082d1b93b06f4fdb57f3b97ef
IbnuSabilGitHub <ibnusabil2301@gmail.com>
2025-02-27 18:03:23 +0800

    apply: animated on RotatingTex

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -5,6 +5,7 @@ import SpotlightCarousel from "../../components/SoptLightCarousel";
 import SplitText from "../../components/SplitText";
 import { useState, useEffect } from "react";
 import RotatingText from "../../components/RotatingText";
+import AnimatedContent from "../../components/AnimatedContent";
 
 function Home() {
   const [baseWidth, setBaseWidth] = useState(() =>
@@ -41,21 +42,30 @@ function Home() {
             rootMargin="-50px"
             onLetterAnimationComplete={handleAnimationComplete}
           />
-          {animationComplete && 
-          <RotatingText
-            texts={["React", "Vite", "Tailwind", "Material Ui", "Vercel"]}
-            mainClassName="px-2 sm:px-2 md:px-3 bg-sky-500 text-neutral-950 overflow-hidden py-0.5 sm:py-1 md:py-0 justify-center rounded-lg"
-            staggerFrom={"last"}
-            initial={{ y: "100%" }}
-            animate={{ y: 0 }}
-            exit={{ y: "-120%" }}
-            staggerDuration={0.025}
-            splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
-            transition={{ type: "spring", damping: 30, stiffness: 400 }}
-            rotationInterval={2000}
-            elementLevelClassName="text-3xl sm:text-3xl md:text-7xl font-bold"
-          />
-        }
+          {animationComplete && (
+            <AnimatedContent
+            distance={150}
+            direction="horizontal"
+            reverse={false}
+            config={{ tension: 80, friction: 20 }}
+            initialOpacity={0.2}
+            animateOpacity
+            >
+              <RotatingText
+                texts={["React", "Vite", "Tailwind", "Material Ui", "Vercel"]}
+                mainClassName="px-2 sm:px-2 md:px-3 bg-sky-500/30 text-sky-500 overflow-hidden py-0.5 sm:py-1 md:py-0 justify-center rounded-lg border border-sky-500"
+                staggerFrom={"last"}
+                initial={{ y: "100%" }}
+                animate={{ y: 0 }}
+                exit={{ y: "-120%" }}
+                staggerDuration={0.025}
+                splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+                transition={{ type: "spring", damping: 30, stiffness: 400 }}
+                rotationInterval={2000}
+                elementLevelClassName="text-3xl sm:text-3xl md:text-7xl font-bold"
+              />
+            </AnimatedContent>
+          )}
         </div>
       </div>
       <div className="flex flex-col items-center justify-center space-y-8 h-[100vh] w-full">

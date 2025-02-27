326b842312d039ebcc551e22e09dd87e9feabe6f
IbnuSabilGitHub <ibnusabil2301@gmail.com>
2025-02-27 17:25:33 +0800

    fix: error ts in rotating text and formating

--- src/components/RotatingText.tsx
+++ src/components/RotatingText.tsx
@@ -1,3 +1,4 @@
+// Sorce Code: https://www.reactbits.dev/text-animations/rotating-text
 import React, {
     forwardRef,
     useCallback,
@@ -70,15 +71,13 @@ import React, {
       const [currentTextIndex, setCurrentTextIndex] = useState<number>(0);
   
       const splitIntoCharacters = (text: string): string[] => {
-        if (typeof Intl !== "undefined" && Intl.Segmenter) {
-          const segmenter = new Intl.Segmenter("en", { granularity: "grapheme" });
-          return Array.from(
-            segmenter.segment(text),
-            (segment) => segment.segment
-          );
+        if (typeof Intl !== "undefined" && (Intl as any)?.Segmenter) {
+          const segmenter = new (Intl as any).Segmenter("en", { granularity: "grapheme" });
+          return Array.from(segmenter.segment(text), (segment: any) => segment.segment);
         }
         return Array.from(text);
       };
+      
   
       const elements = useMemo(() => {
         const currentText: string = texts[currentTextIndex];

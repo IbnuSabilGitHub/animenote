6404e1fc2838da16e581fb3184700b1ede85b169
IbnuSabilGitHub <ibnusabil2301@gmail.com>
2025-03-01 20:15:24 +0800

    apply: titedcard

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,26 +1,55 @@
+import { useEffect, useState } from "react";
+import TiltedCard from "../../components/TiltedCard";
 function About() {
+  const [width, setWidth] = useState(window.innerWidth);
 
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <div>
       <div className="flex items-center justify-center min-h-screen">
-        <figure className="max-w-lg mx-auto">
-          <img
-        className="w-[50rem] rounded-lg"
-        src='https://media.tenor.com/FrXVfMs5tg0AAAAM/apa-coba-haerin.gif'
-        alt='APA COBA'
-        loading="lazy"
+        {width > 768 ? (
+          <TiltedCard
+            imageSrc="https://media.tenor.com/FrXVfMs5tg0AAAAM/apa-coba-haerin.gif"
+            altText="APA COBA!!"
+            captionText="APA COBA!!"
+            containerHeight="400px"
+            containerWidth="400px"
+            imageHeight="400px"
+            imageWidth="400px"
+            rotateAmplitude={12}
+            scaleOnHover={1.1}
+            showTooltip={true}
           />
-          <figcaption className="mt-2 text-xl font-bold text-center text-gray-500 dark:text-gray-400">
-            APA COBA!!
-          </figcaption>
-        </figure>
+        ) : (
+          <figure className="max-w-lg mx-auto">
+            <img
+              className="w-[50rem] rounded-lg"
+              src="https://media.tenor.com/FrXVfMs5tg0AAAAM/apa-coba-haerin.gif"
+              alt="APA COBA"
+              loading="lazy"
+            />
+            <figcaption className="mt-2 text-xl font-bold text-center text-gray-500 dark:text-gray-400">
+              APA COBA!!
+            </figcaption>
+          </figure>
+        )}
       </div>
     </div>
   );
 }
 
 export default About;
-
+// https://media.tenor.com/FrXVfMs5tg0AAAAM/apa-coba-haerin.gif
 // import { useEffect, useState} from "react";
 
 // export default function ProductList() {
